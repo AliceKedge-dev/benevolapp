2eb8e013ec57be5698a1231b6c61968ad2f510ef
Gaspard Lachaize <gaspard.lachaize@kedgebs.com>
2023-06-26 15:20:16 +0200

    routes boutons missions et rouver une activité sur la page home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get '/missions', to: 'missions#mission'
+  get '/mesmissions', to: 'missions#processmission'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
