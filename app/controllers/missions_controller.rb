442dab54c0f4cecb495c23d6c7ca3bb5fd0587e4
Alice Gorius <alice.gorius@kedgebs.com>
2023-06-26 15:19:19 +0200

    profile et home

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,2 +1,25 @@
 class MissionsController < ApplicationController
+
+  def index
+    @missions = Mission.all
+  end
+
+  def new
+    @mission = Mission.new
+  end
+
+  def create
+    @mission = Mission.new(params[:mission])
+    @mission.save
+  end
+
+  def update
+    @mission = Mission.find(params[:id])
+    @mission.save
+  end
+
+  def delete
+    @mission = Mission.find(params[:id])
+    @mission.destroy
+  end
 end
