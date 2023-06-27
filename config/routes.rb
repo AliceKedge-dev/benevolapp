6893779ad9bb9c0e50dd7c22551c6a68977e5a6b
Alice Gorius <alice.gorius@kedgebs.com>
2023-06-27 15:57:09 +0200

    changement

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
     resources :reservations, only: [:new, :create, :show, :index]
   end
 
-
   get "/profile", to: "pages#profile"
-  #get "/mesmissions", to: "missions#mesmissions"
+  get "/mesmissions", to: "missions#mesmissions"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
