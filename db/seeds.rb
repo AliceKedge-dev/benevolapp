2dff81cd4a66ea991a9ced13972e0d14faa43316
Gaspard Lachaize <gaspard.lachaize@kedgebs.com>
2023-06-29 16:55:05 +0200

    modification reservation

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,12 +24,12 @@ asso1 = Asso.new(nom: "spa", category: "animal", email: "spa@gmail.com", passwor
 asso1.photo.attach(io: fileasso1, filename: "jeanvaljean", content_type: "image/png")
 asso1.save
 
-fileasso2 = URI.open("https://res.cloudinary.com/dkx9xgzon/image/upload/t_Profile/v1687960540/development/dpelf17mfup391dqhv9z3bloewq0.png")
+fileasso2 = URI.open("https://res.cloudinary.com/dkx9xgzon/image/upload/v1687879605/production/nyzxt3v9g2qyhps4612onya2n4kh.jpg")
 asso2 = Asso.new(nom: "les restaurants du coeur", category: "social", email: "restauducoeur@gmail.com", password: "password")
 asso2.photo.attach(io: fileasso2, filename: "jeanvaljean", content_type: "image/png")
 asso2.save
 
-fileasso3 = URI.open("https://res.cloudinary.com/dkx9xgzon/image/upload/t_Profile/v1687960542/development/c9dhcnbwlntfv7q8cdfut5gnigg1.png")
+fileasso3 = URI.open("https://res.cloudinary.com/dkx9xgzon/image/upload/v1687879606/production/2upyp7i2xquly4zlsbc8cq5xgtk6.jpg")
 asso3 = Asso.new(nom: "cleanmycalanques", category: "environnement", email: "clean@gmail.com", password: "password")
 asso3.photo.attach(io: fileasso3, filename: "jeanvaljean", content_type: "image/png")
 asso3.save
@@ -37,19 +37,22 @@ asso3.save
 puts "creating missions"
 
 filemission1 = URI.open("https://res.cloudinary.com/dkx9xgzon/image/upload/v1687860070/chien-cage-refuge-spa-full-12643903_sdmz94.jpg")
-mission1 = Mission.new(asso: asso1, date_création: "12/02/2023", nom: "refuge", temps: 2, description: "aide au refuge", localisation: "Quai de Rive Neuve, Marseille", date: "13/06/2023", photo_url: "photo", category: "animal", participants_max: 200, latitude: 43.2922484, longitude: 5.3649312  )
+mission1 = Mission.new(asso: asso1, date_création: "12/02/2023", nom: "refuge", temps: 2, description: "Venez aider un refuge
+  dans ces actions du quotidien,
+  passez du temps avec les animaux dans le besoin !
+  Vous pourrez pour le nettoyage des box, faire des jeux et balade
+  leur donner le bain, nourrir les animaux selon leurs besoins et enfin terminer par une petite séance caresse", localisation: "adresse: Quai de Rive Neuve, Marseille", date: "13/06/2023", photo_url: "photo", category: "animal", participants_max: 200, latitude: 43.2922484, longitude: 5.3649312  )
 mission1.photo.attach(io: filemission1, filename: "spaasso", content_type: "image/png")
 mission1.save
 
-filemission2 = URI.open("https://res.cloudinary.com/dkx9xgzon/image/upload/v1687860601/development/ijh3sa9g69m5bddgnp98g2jikirh.jpg")
-mission2 = Mission.new(asso: asso2, date_création: "12/02/2023", nom: "distribution de nourriture", temps: 3, description: "aide au restos du coeur", localisation: "41 Rue Jobin, Marseille", date: "13/06/2023", photo_url: "photo", category: "environnement", participants_max: 250, latitude: 43.3094971, longitude: 5.3903643  )
+filemission2 = URI.open("https://res.cloudinary.com/dkx9xgzon/image/upload/v1687860071/Duo-riverain_sdf-femme-trottoir-1_eqq7m1.jpg")
+mission2 = Mission.new(asso: asso2, date_création: "12/02/2023", nom: "distribution de nourriture", temps: 3, description: "aide au restos du coeur", localisation: "adresse: 41 Rue Jobin, Marseille", date: "13/06/2023", photo_url: "photo", category: "environnement", participants_max: 250, latitude: 43.3094971, longitude: 5.3903643  )
 mission2.photo.attach(io: filemission2, filename: "foodasso", content_type: "image/png")
 mission2.save
 
 filemission3 = URI.open("https://res.cloudinary.com/dkx9xgzon/image/upload/v1687860070/NETTOYAGES_DE_PLAGE_mwetj6.jpg")
-mission3 = Mission.new(asso: asso3, date_création: "12/02/2023", nom: "nettoyage plage", temps: 2, description: "plage du prado", localisation: "Plages du Prado, Marseille", date: "13/06/2023", photo_url: "photo", category: "social", participants_max: 50, latitude: 43.25540542602539, longitude: 5.374680519104004  )
+mission3 = Mission.new(asso: asso3, date_création: "12/02/2023", nom: "nettoyage plage", temps: 2, description: "plage du prado", localisation: "adresse: Plages du Prado, Marseille", date: "13/06/2023", photo_url: "photo", category: "social", participants_max: 50, latitude: 43.25540542602539, longitude: 5.374680519104004  )
 mission3.photo.attach(io: filemission3, filename: "cleanasso", content_type: "image/png")
 mission3.save
-
 
 puts "AMEN"
