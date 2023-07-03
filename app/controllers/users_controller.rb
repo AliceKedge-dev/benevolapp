cf69858a317d5cf88b4963acdf50dad01cb2b2a8
Alice Gorius <alice.gorius@kedgebs.com>
2023-07-03 09:37:06 +0200

    heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-
   def index
     @users = User.all
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:email, :username, :photo_url, :photo, :password)
   end
 end
