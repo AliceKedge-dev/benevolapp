ef1aa354de3430ee9d599c5f8bf66d02ce6cde87
Alice Gorius <alice.gorius@kedgebs.com>
2023-07-05 11:04:15 +0200

    badges & barre

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,10 +5,8 @@ class PagesController < ApplicationController
       {
         lat: mission.latitude,
         lng: mission.longitude,
-        info_window_html: render_to_string(partial: "info_window",
-        locals: {mission: mission}),
-        marker_html: render_to_string(partial: "marker",
-        locals: {mission: mission})
+        info_window_html: render_to_string(partial: "info_window", locals: { mission: mission }),
+        marker_html: render_to_string(partial: "marker", locals: { mission: mission })
       }
     end
   end
@@ -20,6 +18,9 @@ class PagesController < ApplicationController
 
   def profile
     @chatroom = Chatroom.last
+    current_user.animal_count < 3 ? @url = "badge-patte-bronze.png" : @url = "badge-patte.png"
+    current_user.social_count < 3 ? @url = "badge-social.png" : @url = "badge-social-silver.png"
+    current_user.environnement_count < 3 ? @url = "badge-environnement.png" : @url = "badge-environnement-silver.png"
   end
 
   def index
@@ -33,10 +34,8 @@ class PagesController < ApplicationController
     {
       lat: mission.latitude,
       lng: mission.longitude,
-      info_window_html: render_to_string(partial: "info_window",
-      locals: {mission: mission}),
-      marker_html: render_to_string(partial: "marker",
-      locals: {mission: mission})
+      info_window_html: render_to_string(partial: "info_window", locals: { mission: mission }),
+      marker_html: render_to_string(partial: "marker", locals: { mission: mission })
     }
     end
   end
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,19 +1,27 @@
 class ReservationsController < ApplicationController
-
   def index
     @reservations = Reservation.all
   end
 
   def create
+    mission = Mission.find(params[:mission_id])
     @reservation = Reservation.new
     @reservation.user = current_user
-    @reservation.mission = Mission.find(params[:mission_id])
+    @reservation.mission = mission
+
     if @reservation.save!
+      case mission.category
+      when "animal"
+        current_user.animal_count += 1
+      when "social"
+        current_user.social_count += 1
+      when "environnement"
+        current_user.environnement_count += 1
+      end
+      current_user.save!
       redirect_to confirmation_path(mission_id: @reservation.mission.id), notice: "La reservation a été créé avec succès."
     else
       render :new
     end
   end
-
-
 end
